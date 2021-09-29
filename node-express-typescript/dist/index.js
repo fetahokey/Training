25f34607f02c14fecba4dbddf4c89fb09f8accf9
fetahokey <ouldkradda.fateh@gmail.com>
2021-09-29 09:52:51 +0100

    Secure my Node.js application

--- node-express-typescript/dist/index.js
+++ node-express-typescript/dist/index.js
@@ -3,10 +3,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-const port = 8080;
+// intitialize configuration
+dotenv_1.default.config();
+// port now is available to Node.js runtime
+// as if it where an environment variable
+const port = process.env.SERVER_PORT;
 // configure Express to use EJS
 app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
 app.set('views', path_1.default.join(__dirname, 'views'));
