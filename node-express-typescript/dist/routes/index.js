b1824423c290ed6fe3aadb0462e102554d52fa39
fetahokey <ouldkradda.fateh@gmail.com>
2021-09-29 12:59:45 +0100

    take advantage of the user profile information returned from Okta

--- node-express-typescript/dist/routes/index.js
+++ node-express-typescript/dist/routes/index.js
@@ -3,26 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.register = void 0;
 const register = (app) => {
     const oidc = app.locals.oidc;
-    // define a route handler for the default home
+    // define a route handler for the default home page
     app.get('/', (req, res) => {
-        // tslint:disable-next-line:no-console
-        console.log(`Is here !! `);
-        res.render('index');
+        const user = req.useContext ? req.useContext.userInfo : null;
+        res.render('index', { isAuthenticated: req.isAuthenticated(), user });
     });
-    // define a secure route handler for the login page thar redirect to /inventory
+    // define a secure handler route for the login page that redirects to /inventory
     app.get('/login', oidc.ensureAuthenticated(), (req, res) => {
         res.redirect('/inventory');
     });
-    // define a route to handle a logout
+    // define route to handle logout
     app.get('/logout', (req, res) => {
         req.logout();
         res.redirect('/');
     });
     // define a secure route handler for the inventory page
     app.get('/inventory', oidc.ensureAuthenticated(), (req, res) => {
-        // tslint:disable-next-line:no-console
-        console.log(`Is here !! `);
-        res.render('inventory');
+        const user = req.useContext ? req.useContext.userInfo : null;
+        res.render('/inventory', { isAuthenticated: req.isAuthenticated, user });
     });
 };
 exports.register = register;
--- node-express-typescript/src/routes/index.ts
+++ node-express-typescript/src/routes/index.ts
@@ -1,3 +1,4 @@
+/*
 import * as express from 'express';
 
 export const register = ( app: express.Application) => {
@@ -22,5 +23,34 @@ export const register = ( app: express.Application) => {
     // define a secure route handler for the inventory page
     app.get('/inventory', oidc.ensureAuthenticated(), (req:any,res)=>{
         res.render('inventory');
+    })
+}
+*/
+import * as express from 'express';
+
+export const register = (app: express.Application) =>{
+    const oidc = app.locals.oidc;
+
+    // define a route handler for the default home page
+    app.get('/', (req:any, res)=>{
+        const user = req.useContext ? req.useContext.userInfo : null;
+        res.render('index', {isAuthenticated: req.isAuthenticated(), user});
+    });
+
+    // define a secure handler route for the login page that redirects to /inventory
+    app.get('/login', oidc.ensureAuthenticated(), (req,res)=>{
+        res.redirect('/inventory');
+    });
+
+    // define route to handle logout
+    app.get('/logout', (req:any, res)=>{
+        req.logout();
+        res.redirect('/');
+    });
+
+    // define a secure route handler for the inventory page
+    app.get('/inventory',oidc.ensureAuthenticated() ,(req:any, res)=>{
+        const user = req.useContext ? req.useContext.userInfo: null;
+        res.render('/inventory', {isAuthenticated: req.isAuthenticated, user});
     })
 }
