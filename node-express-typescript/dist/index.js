4de1cf3f52b33be129482f964377bf2b0906c819
fetahokey <ouldkradda.fateh@gmail.com>
2021-09-29 03:02:34 +0100

    Add an asset build script for Typescript

--- node-express-typescript/dist/index.js
+++ node-express-typescript/dist/index.js
@@ -4,11 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 8080;
+// configure Express to use EJS
+app.use(express_1.default.static(path_1.default.join(__dirname, 'public')));
+app.set('views', path_1.default.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 // define a route handler for the default home page
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    // render the index template
+    res.render('index');
 });
 // start the Express server
 app.listen(port, () => {
--- node-express-typescript/src/index.ts
+++ node-express-typescript/src/index.ts
@@ -1,16 +1,22 @@
-import express from "express";
+import express from 'express';
+import path from 'path';
 const app = express();
 const port = 8080;
 
-// define a route handler for the default home page
+// configure Express to use EJS
 
+app.use(express.static( path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// define a route handler for the default home page
 app.get('/', (req, res)=>{
-    res.send('Hello World');
+    // render the index template
+    res.render('index');
 });
 
 
 // start the Express server
-
 app.listen(port, ()=>{
     // tslint:disable-next-line:no-console
     console.log(`Server started at http://localhost:${ port }`);
