23fc9a9c2ca0e4edcb0218c273381278f52b4a44
fetahokey <ouldkradda.fateh@gmail.com>
2021-09-29 09:01:22 +0100

    A Better Way to Manage Configuration Settings in Node.js

--- node-express-typescript/src/index.ts
+++ node-express-typescript/src/index.ts
@@ -1,10 +1,17 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
 const app = express();
-const port = 8080;
+
+
+// intitialize configuration
+dotenv.config();
+
+// port now is available to Node.js runtime
+// as if it where an environment variable
+const port = process.env.SERVER_PORT;
 
 // configure Express to use EJS
-
 app.use(express.static( path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
