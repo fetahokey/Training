118f6f7dc932d678108f492ba8f5bb23cac1217b
fetahokey <ouldkradda.fateh@gmail.com>
2021-09-29 16:39:22 +0100

    include the new api module

--- node-express-typescript/src/routes/index.ts
+++ node-express-typescript/src/routes/index.ts
@@ -1,32 +1,5 @@
-/*
 import * as express from 'express';
-
-export const register = ( app: express.Application) => {
-    const oidc = app.locals.oidc;
-
-    // define a route handler for the default home
-    app.get('/', (req:any, res)=>{
-        res.render('index');
-    });
-
-    // define a secure route handler for the login page thar redirect to /inventory
-    app.get('/login', oidc.ensureAuthenticated(), (req,res)=>{
-        res.redirect('/inventory');
-    })
-
-    // define a route to handle a logout
-    app.get('/logout', (req:any,res)=>{
-        req.logout();
-        res.redirect('/');
-    });
-
-    // define a secure route handler for the inventory page
-    app.get('/inventory', oidc.ensureAuthenticated(), (req:any,res)=>{
-        res.render('inventory');
-    })
-}
-*/
-import * as express from 'express';
+import * as api from './api';
 
 export const register = (app: express.Application) =>{
     const oidc = app.locals.oidc;
@@ -59,4 +32,7 @@ export const register = (app: express.Application) =>{
 
         res.render('inventory', {isAuthenticated: req.isAuthenticated(), user});
     })
+
+    // include the new api module
+    api.register( app ) ;
 }
