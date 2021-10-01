79b6b8f5473c8208bd9473dd649979476fa61184
fetahokey <ouldkradda.fateh@gmail.com>
2021-10-01 19:36:16 +0100

    add dummy data

---
+++ react-admin-dashboard/src/dummyData.js
@@ -0,0 +1,247 @@
+
+export const salesData = [
+    {
+      name: 'Day 1',
+      sales: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Day 2',
+      sales: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Day 3',
+      sales: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Day 4',
+      sales: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Day 5',
+      sales: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Day 6',
+      sales: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Day 7',
+      sales: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+    {
+        name: 'Day 8',
+        sales: 300,
+        pv: 4300,
+        amt: 2100,
+      },
+      {
+        name: 'Day 9',
+        sales: 389,
+        pv: 4300,
+        amt: 2100,
+      },
+      {
+        name: 'Day 9',
+        sales: 1410,
+        pv: 4300,
+        amt: 2100,
+      },
+      {
+        name: 'Day 10',
+        sales: 4010,
+        pv: 4300,
+        amt: 2100,
+      },
+  ];
+
+
+  
+export const productData = [
+  {
+    name: 'Day 1',
+    sales: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Day 2',
+    sales: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Day 3',
+    sales: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Day 4',
+    sales: 2780,
+    pv: 3908,
+    amt: 2000,
+  }
+];
+
+
+  export const userRows = [
+    { 
+        id: 1, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 2, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 3, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 4, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 5, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 6, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 7, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 8, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 9, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },{ 
+        id: 10, 
+        username: 'Neymar Jr', 
+        avatar: 'https://i.pravatar.cc/150', 
+        email: "Neymar.jr11@gmail.com" ,
+        status: "active",
+        transaction: "3 500 DZD" 
+    },
+] ;
+
+export const productRows = [
+  { 
+      id: 1, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 2, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 3, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 4, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 5, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 6, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 7, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 8, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 9, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },{ 
+      id: 10, 
+      name: 'Samsung GALAXY SX', 
+      img: 'https://i.pravatar.cc/150', 
+      stock: 5351 ,
+      status: "active",
+      price: "3 500 DZD" 
+  },
+] ;
